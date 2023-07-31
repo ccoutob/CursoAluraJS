0e7831d680fe4ac7ba82e38f90eeb0d72398b730
ccoutob <rm97755@fiap.com.br>
2023-07-31 12:54:39 -0300

    67% do curso concluido

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Utilizando querySelector em JS:
 querySelector('.tecla'); -> para chamar uma classe
 querySelector('#tecla'); -> para chamar um id
 querySelector('tecla'); -> para chamar a tag
+
--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSom;
+
+let contador = 0;
+let tecla
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador] //otimizando a estrutura do código
+    const instrumento = tecla.classList[1] //instrumento recebe a classe das teclas ("Pom, Clap, Psh")
+    const idAudio = `#som_${instrumento}` //idAudio recebe o som da tecla que foi chamada pelo instrumento, usando "$" para aplicar a váriavel dentro da String
+
+    //console.log(instrumento)
+
+    tecla.onclick = function () {
+        tocaSom(idAudio); //Ao clicar na tecla retorna o audio do seu respectivo som
+    }
+
+    contador ++
+    //console.log(contador)
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
 /* Utilizamos o nome da função sem os "()" para que não seja executada assim que o usuário interaja com a página*/

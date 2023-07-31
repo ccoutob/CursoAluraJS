f923bc27b62485909911f3301a492325e637db3d
ccoutob <rm97755@fiap.com.br>
2023-07-31 17:06:39 -0300

    Curso Finalizado

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio)
+
+    if (elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSom;
 
-let contador = 0;
-let tecla
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador] //otimizando a estrutura do código
+    const instrumento = tecla.classList[1] //instrumento recebe a classe das teclas ("Pom, Clap, Psh")
+    const idAudio = `#som_${instrumento}` //idAudio recebe o som da tecla que foi chamada pelo instrumento, usando "$" para aplicar a váriavel dentro da String
+
+    tecla.onclick = function () {
+        tocaSom(idAudio); //Ao clicar na tecla retorna o audio do seu respectivo som
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+
+/* while (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador] //otimizando a estrutura do código
     const instrumento = tecla.classList[1] //instrumento recebe a classe das teclas ("Pom, Clap, Psh")
@@ -23,7 +49,7 @@ while (contador < listaDeTeclas.length) {
 
     contador ++
     //console.log(contador)
-}
+} */
 
 
 
